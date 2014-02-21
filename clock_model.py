0e17dc554f35488f260d4887d2628cf8980a92d8
dtctht <neverneverforever@gmail.com>
2014-02-21 16:31:05 +0800

    commented out print

--- clock_model.py
+++ clock_model.py
@@ -35,7 +35,7 @@ class clocker(Canvas):
         #self.minute()
         #self.second()
     ##
-        print(self.π)
+        print('π is : %f' % self.π)
     ## 第二种方法实现，每秒（实际是999毫秒）读取当前时间，并转换成时分秒针位置坐标，刷新界面上时钟时分秒针位置
         self.modelTwo()
 
@@ -44,7 +44,6 @@ class clocker(Canvas):
         self.setNumber()                    ##初始化时钟刻度盘大刻度
     ## 第二种方法，实时读取当前系统时间刷新钟面
     def modelTwo(self):
-        print ('Another way to come up!')
         self.sm.set(strftime('%H:%M:%S'))
         self.setTime()
         self.create_line(self.h,fill='gray',width=20)
@@ -53,7 +52,7 @@ class clocker(Canvas):
         self.master.after(999,self.modelTwo)
     ## 初始化表盘大刻度
     def setNumber(self):
-        print (self.r)
+        ###print (self.r)
         ns = list(self.r) #获得基准坐标，秒针最长，取得表针坐标用以绘制刻度
         sms = ('ns is initiated to: %s' % ns)
         self.sm.set(sms)
@@ -61,20 +60,18 @@ class clocker(Canvas):
         nsCut =[ns[0],ns[1],ns[2],ns[3]+ws]
         na = math.pi/6
         for char in 'kiss me baby':
-            print (char)
-            print (ns)
             na = na + math.pi/6
             ns = self.getCoords(ns,na)
-            print('after rotate pi/6 ns is %s' % ns)
+            ###print('after rotate pi/6 ns is %s' % ns)
             nsCut = self.getCoords(nsCut,na)
-            print(('after rotate pi/6 nsCut is %s' % nsCut))
+            ###print(('after rotate pi/6 nsCut is %s' % nsCut))
             nsNew = [nsCut[2],nsCut[3],ns[2],ns[3]]
-            print(nsNew)
+            ###print(nsNew)
             self.create_line(nsNew,fill='black',width = 25)
             self.create_line(nsNew,fill='white',width = 1)
     ## 初始化表盘小刻度
     def setDial2(self):
-        print (self.r)
+        ### print (self.r)
         ns = list(self.r) #获得基准坐标，秒针最长，取得表针坐标用以绘制刻度
         sms = ('ns is initiated to: %s' % ns)
         self.sm.set(sms)
@@ -82,15 +79,15 @@ class clocker(Canvas):
         nsCut =[ns[0],ns[1],ns[2],ns[3]+ws]
         na = math.pi/30
         for i in range(60):
-            print (i)
-            print (ns)
+            ###print (i)
+            ###print (ns)
             na = na + math.pi/30
             ns = self.getCoords(ns,na)
-            print('after spin pi/6 ns is %s' % ns)
+            ###print('after spin pi/6 ns is %s' % ns)
             nsCut = self.getCoords(nsCut,na)
-            print(('after spin pi/6 nsCut is %s' % nsCut))
+            ###print(('after spin pi/6 nsCut is %s' % nsCut))
             nsNew = [nsCut[2],nsCut[3],ns[2],ns[3]]
-            print(nsNew)
+            ###print(nsNew)
             self.create_line(nsNew,fill='gray',width = 16)
             #self.create_line(nsNew,fill='white',width = 1)
     ## 转动秒针，每次（秒）旋转6度
@@ -118,7 +115,7 @@ class clocker(Canvas):
         self.h = self.setCoords(self.h,x,20)
         hrHand = self.create_line(self.h,fill='black',width=20)
         self.master.after(1000, self.hour)
-        print('hour hand displayed?\n')
+        ###print('hour hand displayed?\n')
     ##
     # ## getTimeChange(self):
     ## 当前秒读数curs为输入参数，扫描当前时间变化，当秒变化，返回当前秒读数。未实际未调用
@@ -131,9 +128,6 @@ class clocker(Canvas):
         while gTime[2] == self.curs:
             gTime = strftime('%H:%M:%S')
             gTime = gTime.split(':')
-            print(gTime)
-            count = count + 1
-        print('Count is : %d' % count)
         self.curs = gTime[2]
         #self.master.after(999,self.getTimeChange)
     ## 刷新时分秒针位置，现将当前时分秒针位置擦除，转动时分秒针
@@ -152,7 +146,7 @@ class clocker(Canvas):
         hha = math.pi * 2 *( (hh % 12) / 12 + mm/(60*12) + ss/(60*60*12))- math.pi/2 #时针偏离基准（x轴正方向)的角度
         mma = math.pi * 2 * (mm + ss/60)/60- math.pi/2 #分针偏离基准（x轴正方向)的角度
         ssa = math.pi * 2 * ss / 60 - math.pi/2 #秒针偏离基准（x轴正方向)的角度
-        print (hha, mma, ssa)
+        ###print (hha, mma, ssa)
         self.h = self.getCoords(self.h,hha)    #时针新坐标
         self.m = self.getCoords(self.m,mma)    #分针新坐标
         self.s = self.getCoords(self.s,ssa)    #秒针新坐标
@@ -167,7 +161,7 @@ class clocker(Canvas):
         y = r * math.sin(a)
         cc[2]=x+cc[0]
         cc[3]=y+cc[1]
-        print ('get the coords of hand: %s' % cc)
+        ###print ('get the coords of hand: %s' % cc)
         return (cc)
 
     ## 输入当前表针起始坐标cc、旋转角a，和表针粗细ww,返回表针旋转后的坐标（线段起始坐标）
@@ -177,36 +171,31 @@ class clocker(Canvas):
     def setCoords(self,cc,a, ww):
             self.create_line(cc,fill="white",width=ww)
             r = ( (cc[2] - cc[0]) ** 2 + (cc[3] - cc[1] ) ** 2 ) ** 0.5
-            print('current R is:%8.4f' % r)
-            print('angle is %8f'% a)
-            print (cc)
+            ###print('current R is:%8.4f' % r)
+            ###print('angle is %8f'% a)
+            ###print (cc)
             x = cc[2] - cc[0]
             y = cc[3] - cc[1]
-            print(x,y)
+            ###print(x,y)
             sina = y / r
             cosa = x / r
             starta = float()
             if y > 0 and x > 0:
                 starta=math.asin ( sina ) ## 0 ~ PI/2
-                print ("q1: %f" % starta)
             else:
                 if  y > 0 and x <=0:
                     starta = math.acos(cosa) ## PI/2 ~ PI
-                    print ("q2: %f" % starta)
                 else:
                     if y <=0 and x <=0:
                         starta = math.pi*2 - math.acos(cosa)
-                        print("q3: %f" % starta)
                     else:
                         starta = math.pi * 2 + math.asin(sina)
-                        print("q4: %f" % starta)
-            print (starta)
+            ###print (starta)
             a = starta + a
-            print("new a is: %f" % a)
+            ###print("new a is: %f" % a)
             cc[2] = cc[0] + r * math.cos(a)
             cc[3] = cc[1] + r * math.sin(a)
-            print ('after spin, R is %8f\n' % r)
-            print(cc)
+            ###print ('after spin, R is %8f\n' % r)
             return (cc)
     def tick(self):
         #self.master.title(strftime('%H:%M:%S'))
